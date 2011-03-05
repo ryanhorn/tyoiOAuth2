94e739401525bb51aeb849a34e375c29672650e1
Ryan Horn <ryan.horn.web@gmail.com>
2011-03-04 21:13:13 -0800

    Change client constructor to include all possible arguments

--- tests/test_oauth2.py
+++ tests/test_oauth2.py
@@ -10,5 +10,23 @@ class TestClient(unittest.TestCase):
         self._mox = mox.Mox()
 
     def testNewClientBadGrantType(self):
-        self.assertRaises(UnsupportedGrantTypeError, Client,
-                          grant_type='bad_grant_type')
+        self.assertRaises(UnsupportedGrantTypeError, Client, client_id='test',
+                          client_secret='test', access_token_endpoint='test',
+                          auth_endpoint='test', grant_type='bad_grant_type')
+
+    def testNewClientValidParams(self):
+        client = Client(client_id='test_client_id',
+                        client_secret='test_client_secret',
+                        access_token_endpoint='test_access_token_endpoint',
+                        auth_endpoint='test_auth_endpoint',
+                        grant_type='authorization_code',
+                        redirect_uri='test_redirect_uri',
+                        scope=['test_scope_1', 'test_scope_2'])
+
+        self.assertEquals('test_client_id', client._client_id)
+        self.assertEquals('test_client_secret', client._client_secret)
+        self.assertEquals('test_access_token_endpoint', client._access_token_endpoint)
+        self.assertEquals('test_auth_endpoint', client._auth_endpoint)
+        self.assertEquals('authorization_code', client._grant_type)
+        self.assertEquals('test_redirect_uri', client._redirect_uri)
+        self.assertEquals(['test_scope_1', 'test_scope_2'], client._scope)
--- tyoi/oauth2/__init__.py
+++ tyoi/oauth2/__init__.py
@@ -3,7 +3,16 @@ class UnsupportedGrantTypeError(Exception):
 
 
 class Client(object):
-    def __init__(self, grant_type):
+    def __init__(self, client_id, client_secret, grant_type,
+                 access_token_endpoint, auth_endpoint, redirect_uri=None,
+                 scope=None):
         if grant_type not in ('authorization_code', 'client_credentials'):
             raise UnsupportedGrantTypeError('Unsupported grant type "%s"' %
                                             grant_type)
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._grant_type = grant_type
+        self._access_token_endpoint = access_token_endpoint
+        self._auth_endpoint = auth_endpoint
+        self._redirect_uri = redirect_uri
+        self._scope = scope
