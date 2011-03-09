e36780f204b0dd9f8a7ec96cb731db0d4e13fcd4
Ryan Horn <ryan.horn.web@gmail.com>
2011-03-09 13:03:53 -0800

    README placeholder and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="tyoi.OAuth2",
+    version="0.1.0",
+    author="Ryan Horn",
+    author_email="ryan.horn.web@gmail.com",
+    description=("Implements the client side of the OAuth 2 protocol"),
+    keywords="oauth oauth2 auth authentication",
+    url="https://github.com/ryanhorn/tyoiOAuth2",
+    packages=["tyoi", "tyoi.oauth2"],
+    test_suite="tests",
+    tests_requre=["mox"]
+)
