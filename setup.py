b764ac1c55fb5d4b7f155fdddce87bd03aef6899
Ryan Horn <ryan.horn.web@gmail.com>
2011-03-11 19:29:44 -0800

    Updated setup.py with new version and packages

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="tyoi.OAuth2",
-    version="0.1.0",
+    version="0.2.0",
     author="Ryan Horn",
     author_email="ryan.horn.web@gmail.com",
     description=("Implements the client side of the OAuth 2 protocol"),
     keywords="oauth oauth2 auth authentication",
     url="https://github.com/ryanhorn/tyoiOAuth2",
-    packages=["tyoi", "tyoi.oauth2"],
+    packages=["tyoi", "tyoi.oauth2", "tyoi.oauth2.grants", "tyoi.oauth2.authenticators"],
     test_suite="tests",
     tests_require=["mox"]
 )
